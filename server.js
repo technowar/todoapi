90a53dd966a607c0c7d5d511763eab8c7217cd44
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2014-10-31 06:44:42 +0000

    Refactor

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ var Hapi = require('hapi');
 var Good = require('good');
 var Logs = require('./logs/logs');
 
-var server = new Hapi.Server('127.0.0.1', 3100);
+var server = new Hapi.Server(3100);
+
+server.route({
+	method : 'GET',
+	path   : '/',
+
+	handler: function (request, reply) {
+		reply('Hello, world!');
+	}
+});
 
 server.pack.register({
 	'plugin'  : Good,
