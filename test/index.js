8aedf9178a1e2aed35b2f3571216bed5fc5cf972
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2015-01-13 04:49:36 +0000

    Refactor test

--- test/index.js
+++ test/index.js
@@ -4,13 +4,13 @@ var Lab = exports.lab = require('lab').script();
 var Code = require('code');
 var Hapi = require('hapi');
 
-var Server = new Hapi.Server();
+Lab.describe('`Plugins` Test', function () {
+	var Server = new Hapi.Server();
 
-Lab.describe('`Hapi.Pack` Test', function () {
 	Lab.it('should have `Good` plugin', function (done) {
 		Server.register({
 			'register' : require('good'),
-			'options' : require('../../log/')
+			'options' : require('../log/')
 		}, function (error) {
 			Code.expect(error).to.not.exist;
 
@@ -30,7 +30,7 @@ Lab.describe('`Hapi.Pack` Test', function () {
 
 	Lab.it('should have `api` plugin', function (done) {
 		Server.register({
-			'register' : require('../../api')
+			'register' : require('../api')
 		}, function (error) {
 			Code.expect(error).to.not.exist;
 
